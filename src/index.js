c43c407f4e2ccf1808c2eac75d754729988326f2
Kontuch Suksawat <kontuch@localhost.localdomain>
2024-12-19 00:34:36 +0700

    add rounte

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./pages/home";
+import About from "./pages/about";
+import Blog from "./pages/blog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Blog",
+    element: <Blog />,
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const showTime = () => {
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
-}
+};
 
-setInterval(showTime,1000);
+setInterval(showTime, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
